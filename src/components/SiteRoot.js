c865d2418234986a27e79352ac1d26e9889c8455
Matthew Kuhn <matthewkuhn55@gmail.com>
2020-11-14 15:37:27 -0500

    Add some paths

--- src/components/SiteRoot.js
+++ src/components/SiteRoot.js
@@ -6,7 +6,16 @@ export default class SiteRoot extends React.Component {
   render() {
     return (
       <Router>
-        <Route path="/" render={(props) => <HomePage {...props} />} />
+        <Route
+          path="/"
+          exact={true}
+          render={(props) => <HomePage {...props} />}
+        />
+        <Route path="/profile/:userId" exact={true} />
+        <Route path="/reviews" exact={true} />
+        <Route path="/polls" exact={true} />
+        <Route path="/login" exact={true} />
+        <Route path="/register" exact={true} />
       </Router>
     );
   }
