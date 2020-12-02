0e739538f84e0827383352d02a93efe8e63ad158
Matthew Kuhn <matthewkuhn55@gmail.com>
2020-12-02 10:45:51 -0500

    remove review box if no one logged in

--- src/project/SearchDetails/MovieDetail.js
+++ src/project/SearchDetails/MovieDetail.js
@@ -1,7 +1,10 @@
 import React from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import {createReview, findReviewsForMovie} from "../../services/reviewService";
+import {
+  createReview,
+  findReviewsForMovie,
+} from "../../services/reviewService";
+import { getSessionUser } from "../../services/userService";
 
 class MovieDetail extends React.Component {
   constructor(props) {
@@ -10,7 +13,8 @@ class MovieDetail extends React.Component {
     this.state = {
       movie: "",
       review: "",
-      fetchedReviews: []
+      fetchedReviews: [],
+      user: { username: "" },
     };
   }
 
@@ -18,38 +22,49 @@ class MovieDetail extends React.Component {
     axios
       .get(`https://www.omdbapi.com/?apikey=4dc3a14a&i=${this.props.movieId}`)
       .then((response) =>
-          this.setState({ movie: response.data },
-              () => findReviewsForMovie(this.state.movie.imdbID).then(reviews => this.setState({fetchedReviews: reviews}))
-          ));
+        this.setState({ movie: response.data }, () =>
+          findReviewsForMovie(this.state.movie.imdbID).then((reviews) =>
+            this.setState({ fetchedReviews: reviews })
+          )
+        )
+      );
+    getSessionUser()
+      .then((response) => {
+        if (response.status !== 200) {
+        } else {
+          return response.json();
+        }
+      })
+      .then((user) => this.setState({ user: { username: user.username } }))
+      .catch((error) => {});
   }
 
   setReviewText = (evt) => {
-    this.setState({review: evt.target.value})
-  }
+    this.setState({ review: evt.target.value });
+  };
 
   addReview = () => {
     // todo
     // this is probably not correct behavior, you don't want the review box or button to
     // even show up if you're not logged in
-    console.log(this.state.review)
-    console.log(this.state.movie.imdbID)
+    console.log(this.state.review);
+    console.log(this.state.movie.imdbID);
     createReview(this.state.review, this.state.movie.imdbID)
-        .then(response => {
-          if(response.status !== 200) {
-            throw new Error("login before adding a review")
-          } else {
-            return findReviewsForMovie(this.state.movie.imdbID)
-          }
-        })
-        .then(reviews => this.setState({fetchedReviews: reviews}))
-        .catch(error => alert(error))
-  }
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error("login before adding a review");
+        } else {
+          return findReviewsForMovie(this.state.movie.imdbID);
+        }
+      })
+      .then((reviews) => this.setState({ fetchedReviews: reviews }))
+      .catch((error) => alert(error));
+  };
 
   render() {
     return (
       <div className="d-flex justify-content-center fill text-white">
         <div className="col-8">
-          <Link to={"/"}>Home</Link>
           <h1 className="d-flex justify-content-center">
             {this.state.movie.Title}
           </h1>
@@ -63,15 +78,28 @@ class MovieDetail extends React.Component {
           <p>Director: {this.state.movie.Director}</p>
           <p>Actors: {this.state.movie.Actors}</p>
           <p>Summary: {this.state.movie.Plot}</p>
-          <textarea className="form-control" rows="3" onChange={this.setReviewText}
-                    placeholder="write a review here">{this.state.review}</textarea>
-          <button className="btn btn=primary" onClick={this.addReview}>Add Review</button>
+          {this.state.user.username !== "" && (
+            <div className="row">
+              <textarea
+                className="form-control col-8"
+                rows="3"
+                onChange={this.setReviewText}
+                placeholder="write a review here"
+              >
+                {this.state.review}
+              </textarea>
+              <button
+                className="btn btn-primary col-3"
+                onClick={this.addReview}
+              >
+                Add Review
+              </button>
+            </div>
+          )}
           <ul>
-            {
-              this.state.fetchedReviews.map(review => (
-                  <li>{review.text}</li>
-              ))
-            }
+            {this.state.fetchedReviews.map((review) => (
+              <li>{review.text}</li>
+            ))}
           </ul>
         </div>
       </div>
