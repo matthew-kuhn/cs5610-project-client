2dd3984dfbae6c35f32653018c436efc6d97e1a7
Matthew Kuhn <matthewkuhn55@gmail.com>
2020-12-10 15:43:51 -0500

    Fix logout big on homescreen

--- src/services/urlRoot.js
+++ src/services/urlRoot.js
@@ -1,1 +1,2 @@
-export const url = "https://polar-shelf-40496.herokuapp.com"
+// export const url = "https://polar-shelf-40496.herokuapp.com"
+export const url = "http://localhost:8080";
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,43 +1,42 @@
-import {url} from "./urlRoot";
+import { url } from "./urlRoot";
 
 export const register = (username, password, role, adminKey, name) =>
-    fetch(`${url}/api/register`, {
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify({username, password, role, adminKey, name}),
-        headers: {"content-type": "application/json"},
-    }).then((response) => response.json());
+  fetch(`${url}/api/register`, {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify({ username, password, role, adminKey, name }),
+    headers: { "content-type": "application/json" },
+  }).then((response) => response.json());
 
 export const login = (username, password) =>
-    fetch(`${url}/api/login`, {
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify({username, password}),
-        headers: {
-            "content-type": "application/json",
-        },
-    });
+  fetch(`${url}/api/login`, {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify({ username, password }),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
 
 export const getSessionUser = () =>
-    fetch(`${url}/api/currentUser`, {
-        credentials: "include",
-    });
+  fetch(`${url}/api/currentUser`, {
+    credentials: "include",
+  });
 
 export const getUser = (username) =>
-    fetch(`${url}/api/users/${username}`, {
-        credentials: "include",
-    });
+  fetch(`${url}/api/users/${username}`, {
+    credentials: "include",
+  });
 
 export const logOut = () => {
-    fetch(`${url}/api/logout`, {
-        method: "POST",
-        credentials: "include",
-    });
+  return fetch(`${url}/api/logout`, {
+    method: "POST",
+    credentials: "include",
+  });
 };
 
 export const blockUser = (username) =>
-    fetch(`${url}/api/users/${username}`, {
-        method: "PUT",
-        credentials: "include"
-    })
-        .then(response => response.json())
+  fetch(`${url}/api/users/${username}`, {
+    method: "PUT",
+    credentials: "include",
+  }).then((response) => response.json());
