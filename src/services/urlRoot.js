5dbe7a7ced6ffd9c3bc7cb891a335c10ed1f72c8
jack6177733651 <40873770+jack6177733651@users.noreply.github.com>
2020-12-14 16:53:54 -0500

    Change to heroku config

--- src/services/urlRoot.js
+++ src/services/urlRoot.js
@@ -1,2 +1,2 @@
-// export const url = "https://polar-shelf-40496.herokuapp.com"
-export const url = "http://localhost:8080";
+export const url = "https://polar-shelf-40496.herokuapp.com"
+// export const url = "http://localhost:8080";
